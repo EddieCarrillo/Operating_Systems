d83ec961700a1e462f5c184fe0af49ea4f9a157f
Eduardo Carrillo <edcarril@ucsd.edu>
2018-10-22 16:29:55 -0700

    Added better imp to concurrent ll

--- concurrentlinkedlist.c
+++ concurrentlinkedlist.c
@@ -20,7 +20,6 @@ void List_Init(list_t *L){
 
 
 int List_Insert(list_t *L, int key){
-   pthread_mutex_lock(&L->lock);
    node_t *new = malloc(sizeof(node_t));
    if (new == NULL){//malloc() failed
         perror("malloc");
@@ -30,6 +29,8 @@ int List_Insert(list_t *L, int key){
    }
  //Init node and insert to begininning of the list
   new->key = key;
+  //Just lock the critical section 
+  pthread_mutex_lock(&L->lock); 
   new->next = L->head;
   L->head = new;
   pthread_mutex_unlock(&L->lock);
@@ -43,12 +44,13 @@ int List_Insert(list_t *L, int key){
 int List_Lookup(list_t *L, int key){
     pthread_mutex_lock(&L->lock);
     node_t *curr = L->head;
+    int rv = -1;
+
     while (curr){
         //We have found the key
         if (curr->key == key){
           //We are finished so unlock
-            pthread_mutex_unlock(&L->lock);
-            return 0;
+            rv = 0;
         }
        curr = curr->next;
     }
