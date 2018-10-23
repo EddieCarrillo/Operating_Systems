00f669d134730e1a3a1bb233d78e8411356f7ea3
Eduardo Carrillo <edcarril@ucsd.edu>
2018-10-22 20:02:30 -0700

    Added single buffer consumer/producer solution (2 condition vars)

--- prodcons.c
+++ prodcons.c
@@ -5,6 +5,9 @@ int buffer;
 int count = 0; //Initially empty
 int loops; //Must initialize somewhere
 mutex_t mutex;
+cond_t empty, fill;
+
+
 
 
 void put(int value){
@@ -27,9 +30,9 @@ void* producer(void *arg){
     for(i = 0; i <loops; i++){
          pthread_mutex_lock(&mutex);//Doing a put() is a critical section operation
          if (count == 1) //We can't put anymore items in our buffer, therefore we need to wait for other thread to get()
-             pthread_cond_wait(&cond, &mutex) //Pass the address of the mutex so we can let other thread control the lock
+             pthread_cond_wait(&empty, &mutex) //Pass the address of the mutex so we can let other thread control the lock
          put(i); //After putting the item into the list we can signal to the other thread, that it can perform a get()
-         pthread_cond_signal(&cond);
+         pthread_cond_signal(&fill);
          pthread_mutex_unlock(&mutex);//we are finished with the critical section of the code 
          
     }
@@ -41,11 +44,10 @@ void*consumer(void*arg){
    while (1){
        pthread_mutex_lock(&mutex); //critical section
        if (count == 0) //We pass in the address of the lock b/c we are going to release for the other thread.
-           pthread_cond_wait(&cond, &mutex); //we can only get an item if there is something in the container
+           pthread_cond_wait(&fill, &mutex); //we can only get an item if there is something in the container
        int tmp = get();   
-       pthread_cond_signal(&cond);
+       pthread_cond_signal(&empty);
        pthread_mutex_unlock(&mutex); //We have finished the with critical section therefore we can release the lock
        printf("%d\n", tmp);
    }
-
 }
